a7a68e85d557aa7f12a84372a8adcd85e1a23156
INOUE Takuya <inouetakuya5@gmail.com>
2018-02-12 21:53:08 +0900

    Parse rooms.csv

--- lib/hipchat_exporter/room.rb
+++ lib/hipchat_exporter/room.rb
@@ -2,6 +2,28 @@ module HipChatExporter
   class Room
     attr_accessor :id, :name
 
+    class << self
+      def parse_csv
+        CSV.open(rooms_csv_path).map do |row|
+          generate(row)
+        end
+      end
+
+      def generate(csv_row)
+        data = { id: csv_row[0] }
+        data[:name] = csv_row[1] if csv_row[1].present?
+        self.new(data)
+      end
+
+      def rooms_csv_path
+        if ENV['ENV'] == 'test'
+          File.join(HipChat::Exporter.root_path, 'spec/rooms.csv')
+        else
+          File.join(HipChat::Exporter.root_path, 'rooms.csv')
+        end
+      end
+    end
+
     def initialize(id:, name: nil)
       self.id = id
       self.name = name || "room_#{id}"
--- spec/lib/hipchat_exporter/room_spec.rb
+++ spec/lib/hipchat_exporter/room_spec.rb
@@ -1,4 +1,29 @@
 describe HipChatExporter::Room do
+  describe '.parse_csv' do
+    it 'parse csv file and generate rooms' do
+      rooms = HipChatExporter::Room.parse_csv
+      expect(rooms[0].class).to eq HipChatExporter::Room
+    end
+  end
+
+  describe '.generate' do
+    let(:room) { HipChatExporter::Room.generate(csv_row) }
+
+    context 'when name exists' do
+      let(:csv_row) { [123, 'My Room'] }
+
+      it { expect(room.id).to eq 123 }
+      it { expect(room.name).to eq 'My Room' }
+    end
+
+    context 'when name not exist' do
+      let(:csv_row) { [123] }
+
+      it { expect(room.id).to eq 123 }
+      it { expect(room.name).to eq 'room_123' }
+    end
+  end
+
   describe 'initialize' do
     context 'when name exists' do
       let(:room) { HipChatExporter::Room.new(id: 123, name: 'My Room') }
