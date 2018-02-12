150df35d0d3e2592636d914850ca731eea4da1c2
INOUE Takuya <inouetakuya5@gmail.com>
2018-02-12 22:10:29 +0900

    'UTC' is alias to 'Etc/UTC'

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext'
 require 'dotenv/load'
 require 'pry'
 
-ENV['TIMEZONE'] ||= 'UTC'
+ENV['TIMEZONE'] ||= 'Etc/UTC'
 Time.zone = ENV['TIMEZONE']
 
 Dir[File.join(__dir__, '../lib/**/*.rb')].each do |file|
