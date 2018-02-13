3636388dd73edd47a8a5dfc4988fc1b2571525ee
INOUE Takuya <inouetakuya5@gmail.com>
2018-02-14 00:37:47 +0900

    new file: db/migrate/1518532359_create_messages.rb

---
+++ db/migrate/1518532359_create_messages.rb
@@ -0,0 +1,16 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.string :uuid, null: false
+      t.string :sender_mention_name, null: false
+      t.string :sender_name, null: false
+      t.text :body, null: false
+      t.datetime :sent_at, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :messages, :uuid, unique: true
+    add_index :messages, :sent_at
+  end
+end
